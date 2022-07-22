d15e5fdcae3080743c7957563bdeaa1fab9da56c
johnxadams <johnxadams93@gmail.com>
2022-07-22 21:22:37 +0200

    landing page box adjustment

--- src/components/Layout/Landingpage/Landingpage.jsx
+++ src/components/Layout/Landingpage/Landingpage.jsx
@@ -5,6 +5,11 @@ export const Landingpage = () => {
     <>
       <div className="big-box-behind-lp">
         <div className="inside-big-box-lp">
+          <div className="inside-big-box-lp-header">
+            <div className="my-logo-container">Logo</div>
+            <div className="my-location-email">Mail Location</div>
+            <div></div>
+          </div>
           <div className="name-and-skills-container">
             <div className="my-name">John Adams</div>
             <div className="my-skills">
@@ -15,7 +20,12 @@ export const Landingpage = () => {
               </ul>
             </div>
             <div className="my-info-box">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores possimus fugit, iste obcaecati enim eaque velit sit doloribus quos maiores praesentium quibusdam ducimus earum eos blanditiis, nobis amet vero tempore! Cupiditate dolore quos consequatur doloremque accusantium aliquam nulla totam tempora nobis similique a, illo ex dicta. Vel velit deleniti quo!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores possimus fugit, iste obcaecati enim eaque velit sit
+              doloribus quos maiores praesentium quibusdam ducimus earum eos
+              blanditiis, nobis amet vero tempore! Cupiditate dolore quos
+              consequatur doloremque accusantium aliquam nulla totam tempora
+              nobis similique a, illo ex dicta. Vel velit deleniti quo!
             </div>
           </div>
         </div>
