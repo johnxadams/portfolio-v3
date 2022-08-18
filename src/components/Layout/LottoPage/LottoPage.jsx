f9c2789de85964fecb288af5e9769951609e601b
johnxadams <johnxadams93@gmail.com>
2022-08-18 08:56:14 +0200

    learn more removed

--- src/components/Layout/LottoPage/LottoPage.jsx
+++ src/components/Layout/LottoPage/LottoPage.jsx
@@ -1,48 +1,48 @@
 // hooks
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 //components
-import LottoApplication from './LottoApplication/LottoApplication'
-import LottoLearnMore from './LottoLearnMore/LottoLearnMore';
-import LinkToGifPage from '../LinkToGifPage/LinkToGifPage';
+import LottoApplication from "./LottoApplication/LottoApplication";
+import LottoLearnMore from "./LottoLearnMore/LottoLearnMore";
+import LinkToGifPage from "../LinkToGifPage/LinkToGifPage";
 
 export default function LottoPage() {
+  const arrOfEmptyStrings = ["", "", "", "", "", "", ""];
 
-    const arrOfEmptyStrings = ["","","","","","",""];
-    
-    const [lottoState, setLottoState] = useState(arrOfEmptyStrings);
-  
-    const handleLotto = () => {
-      let newArr = []
-      while (newArr.length < 6) {
-        const randomNumber = Math.floor(Math.random() * 49 + 1);
-        if (!newArr.includes(randomNumber)){
-          newArr.push(randomNumber);
-        }
+  const [lottoState, setLottoState] = useState(arrOfEmptyStrings);
+
+  const handleLotto = () => {
+    let newArr = [];
+    while (newArr.length < 6) {
+      const randomNumber = Math.floor(Math.random() * 49 + 1);
+      if (!newArr.includes(randomNumber)) {
+        newArr.push(randomNumber);
       }
-  
-      const superRandomNumber = Math.floor(Math.random() * (10 + 1));
-  
-      newArr.push(superRandomNumber);
-  
-      setLottoState([...newArr])
-      
     }
-  
-    const handleReset = () => {
-      setLottoState(arrOfEmptyStrings)
-    }
-  
-    return (
-      <>     
-          <LottoApplication outputLottoNumbers={lottoState} emptyBoxes={arrOfEmptyStrings}/>
-          <div className="button-container">
-            <button onClick={handleReset}>Reset</button>
-            <button onClick={handleLotto}>Try your Luck</button>
-          </div>  
-          <LottoLearnMore/>
-          <LinkToGifPage/>
-      </>
-  
-    );
-  }
+
+    const superRandomNumber = Math.floor(Math.random() * (10 + 1));
+
+    newArr.push(superRandomNumber);
+
+    setLottoState([...newArr]);
+  };
+
+  const handleReset = () => {
+    setLottoState(arrOfEmptyStrings);
+  };
+
+  return (
+    <>
+      <LottoApplication
+        outputLottoNumbers={lottoState}
+        emptyBoxes={arrOfEmptyStrings}
+      />
+      <div className="button-container">
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleLotto}>Try your Luck</button>
+      </div>
+      {/* <LottoLearnMore/> */}
+      <LinkToGifPage />
+    </>
+  );
+}
